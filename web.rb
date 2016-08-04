0dd5c27c99d4ad67c023897741e51dd582bd1850
Alan Kebert <adkebert@yahoo.com>
2016-08-04 08:56:29 -0400

    implemented remove person

--- web.rb
+++ web.rb
@@ -89,6 +89,37 @@ class SearchPerson < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
+class PromptToRemovePerson < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    erb_template_string = File.read("prompt-to-remove.html.erb")
+    template = ERB.new(erb_template_string)
+    output   = template.result(binding)
+
+    response.body = output
+    response.content_type = "text/html"
+    response.status = 200
+  end
+end
+
+class RemovePerson < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET(request, response)
+    name = request.query["name"] # <<== replace this with getting the name from the request
+
+    # Find the person (you figure out what method to call, some_method_here is wrong)
+    person = $database.search(name)
+    if person
+      $database.delete(person.name)
+    end
+    erb_template_string = File.read("removed.html.erb")
+    template = ERB.new(erb_template_string)
+    output   = template.result(binding)
+
+    response.body = output
+    response.content_type = "text/html"
+    response.status = 200
+  end
+end
+
 server = WEBrick::HTTPServer.new(Port: 3000)
 server.mount "/", HomePage
 server.mount "/prompt-to-add", PromptToAddPerson
@@ -98,4 +129,6 @@ server.mount "/prompt-to-search", PromptToSearchPerson
 server.mount "/search", SearchPerson
 
 # See if you can implement delete!!!
+server.mount "/prompt-to-remove", PromptToRemovePerson
+server.mount "/remove", RemovePerson
 server.start
